c9d5f57fd66fb5de3e6438d2e18d03ae647d2395
gwalkerhome <gwalkerhome@hotmail.com>
2026-04-13 09:57:47 +0200

    Create firebase-config.js
    Providing access to the Google firebase database

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBsYHiZm9SUb_Uhf2xJfz62tk780RI6N3I",
+  authDomain: "revol-2af7b.firebaseapp.com",
+  databaseURL: "https://revol-2af7b-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "revol-2af7b",
+  storageBucket: "revol-2af7b.firebasestorage.app",
+  messagingSenderId: "594716753095",
+  appId: "1:594716753095:web:b54ee856c4247998d552e2"
+};
